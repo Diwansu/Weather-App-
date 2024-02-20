0b262d82fd22374d7e7872bb970e084bdfab19e0
Diwanshu <diwansu531@gmail.com>
2024-02-20 16:26:22 +0530

    Make enter key functional for user friendly

--- weather.js
+++ weather.js
@@ -1,5 +1,45 @@
-//Complete the Weather API Backend part using openweathermap api
 
-// Progression 1: Create a function and fetch data using "fetch" from openweathermap api and display the data as given in reference image.
+ function getData(city) {
+   const date = new Date();
+   let currentDate = `${date}`;
 
+   const Location = document.getElementById("location");
+   const Current = document.getElementById("current");
 
+   fetch(
+     `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=5b9441056bba2ffb0d1da1631c7fc001`
+   )
+     .then((response) => response.json())
+     .then((data) => {
+       console.log(data);
+
+       Location.innerHTML = `
+        <div class="city">${data.name}, ${data.sys.country}</div>
+        <div class="date">${currentDate.slice(0, 15)}</div>
+      `;
+
+       Current.innerHTML = `
+        <div class="temp">${(data.main.temp - 273).toFixed(1)}°C</div>
+        <div class="weather">${data.weather[0].main}</div>
+        <div class="hi-low">${(data.main.temp_max - 273).toFixed(1)}°C / ${(
+         data.main.temp_min - 273
+       ).toFixed(1)}°C</div>
+      `;
+     })
+     .catch((error) => {
+       console.error("Error fetching data:", error);
+     });
+ }
+
+ function handleSearch(event) {
+   if (event.key === "Enter") {
+     const searchInput = event.target.value;
+     getData(searchInput);
+   }
+ }
+
+ const searchInput = document.querySelector(".searchbox");
+ searchInput.addEventListener("keypress", handleSearch);
+
+ 
+ getData("Bangalore");
